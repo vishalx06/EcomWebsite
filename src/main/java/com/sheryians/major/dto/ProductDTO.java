5e535b7682e6359ad0ba56fa45afd29d05446b7b
Vishal Sharma <vishalsharma@Vishals-MacBook-Air.local>
2024-01-17 22:20:49 +0530

    ecom code

---
+++ src/main/java/com/sheryians/major/dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package com.sheryians.major.dto;
+
+import com.sheryians.major.model.Category;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private int categoryId;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+}
